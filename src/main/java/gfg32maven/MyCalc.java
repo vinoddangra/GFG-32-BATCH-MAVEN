e83f84a6d11c6c2d8692f4bb39e89ae71b61d5ba
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-03-16 19:25:45 +0530

    First Commit

---
+++ src/main/java/gfg32maven/MyCalc.java
@@ -0,0 +1,27 @@
+package gfg32maven;
+
+public class MyCalc {
+	
+	public int sum(int a, int b)
+	{
+		return (a+b);
+	}
+	public int diff(int a, int b)
+	{
+		return (a-b);
+	}
+	public int mul(int a, int b)
+	{
+		return (a*b);
+	}
+
+	public static void main(String[] args) {
+
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is :"+calc.sum(20, 10));
+		System.out.println("Diff is :"+calc.diff(20, 10));
+		System.out.println("Mul is :"+calc.mul(20, 10));
+
+	}
+
+}
